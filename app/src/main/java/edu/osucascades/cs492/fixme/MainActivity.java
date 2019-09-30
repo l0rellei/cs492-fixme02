4f4f860727063d557459074da339c22e67bd1d85
Florentina Ilie <ilief@oregonstate.edu>
2019-09-30 09:38:58 -0700

    MainActivity.java: Correct symbol class TextView
    Android widget TextView needs to be imported into the file before it can be implemented

--- app/src/main/java/edu/osucascades/cs492/fixme/MainActivity.java
+++ app/src/main/java/edu/osucascades/cs492/fixme/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
